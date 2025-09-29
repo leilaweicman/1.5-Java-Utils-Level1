d8fa2b346d7cae63fae63a38974da49303d44f6b
leilaweicman <leilaweic@hotmail.com>
2025-09-29 12:33:17 +0200

    feat(exercise5): add fields, constructor and toString to Person

--- src/exercise5/Person.java
+++ src/exercise5/Person.java
@@ -4,4 +4,17 @@ import java.io.Serializable;
 
 public class Person implements Serializable {
 
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + "}";
+    }
+
 }
