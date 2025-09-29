cc682868c8705969f3373a8695a54d8596a21670
leilaweicman <leilaweic@hotmail.com>
2025-09-29 12:36:14 +0200

    feat(exercise5): implement method to serialize Person object with FILE_NAME constant

--- src/exercise5/ObjectSerialization.java
+++ src/exercise5/ObjectSerialization.java
@@ -1,8 +1,23 @@
 package exercise5;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class ObjectSerialization {
 
+    private static final String FILE_NAME = "exercise5" + File.separator + "person.ser";
     public static void main(String[] args) {
 
+    }
+
+    private static void serializePerson(Person person) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            oos.writeObject(person);
+            System.out.println("Person serialized to " + FILE_NAME);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
