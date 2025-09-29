ce0d5cb533d8ca5b4ccbfecd94c2b6199953fad8
leilaweicman <leilaweic@hotmail.com>
2025-09-29 11:27:38 +0200

    feat(exercise4): implement file reading and print contents to console

--- src/exercise4/TxtFileReader.java
+++ src/exercise4/TxtFileReader.java
@@ -1,6 +1,9 @@
 package exercise4;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class TxtFileReader {
 
@@ -21,7 +24,15 @@ public class TxtFileReader {
     }
 
     private static void readFileAndPrint(File file) {
-
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file.");
+            e.printStackTrace();
+        }
     }
 
 }
