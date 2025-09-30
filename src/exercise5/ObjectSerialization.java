babbbf63d5988c41cd73bac15b7059c6ff86acde
leilaweicman <leilaweic@hotmail.com>
2025-09-30 12:25:55 +0200

    refactor(exercise5): simplify ObjectSerialization main by delegating to ObjectSerializer

--- src/exercise5/ObjectSerialization.java
+++ src/exercise5/ObjectSerialization.java
@@ -7,29 +7,12 @@ public class ObjectSerialization {
     private static final String FILE_NAME = "exercise5" + File.separator + "person.ser";
     public static void main(String[] args) {
         Person person = new Person("Alice", 30);
-        serializePerson(person);
 
-        Person deserialized = deserializePerson();
+        ObjectSerializer serializer = new ObjectSerializer(FILE_NAME);
+        serializer.serialize(person);
+        Person deserialized = serializer.deserialize();
+
         System.out.println("Original: " + person);
         System.out.println("Deserialized: " + deserialized);
     }
-
-    private static void serializePerson(Person person) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
-            oos.writeObject(person);
-            System.out.println("Person serialized to " + FILE_NAME);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static Person deserializePerson() {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
-            return (Person) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
 }
