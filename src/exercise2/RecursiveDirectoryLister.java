c1d7cc4d4578c20d71f9e739aa0795b7adabf7ac
leilaweicman <leilaweic@hotmail.com>
2025-09-30 11:19:35 +0200

    refactor(exercise2): simplify RecursiveDirectoryLister main by delegating to service and printer

--- src/exercise2/RecursiveDirectoryLister.java
+++ src/exercise2/RecursiveDirectoryLister.java
@@ -1,9 +1,7 @@
 package exercise2;
 
 import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
+import java.util.List;
 
 public class RecursiveDirectoryLister {
 
@@ -20,29 +18,10 @@ public class RecursiveDirectoryLister {
             return;
         }
 
-        listDirectoryRecursive(dir, 0);
+        RecursiveDirectoryService service = new RecursiveDirectoryService();
+        RecursiveDirectoryPrinter printer = new RecursiveDirectoryPrinter();
+
+        List<FileInfo> files = service.listRecursive(dir, 0);
+        printer.print(files);
     }
-
-    private static void listDirectoryRecursive(File dir, int level) {
-        File[] files = dir.listFiles();
-        if (files == null) return;
-
-        Arrays.sort(files, (f1, f2) -> f1.getName().compareToIgnoreCase(f2.getName()));
-
-        for (File file : files) {
-            printFileInfo(file, level);
-            if (file.isDirectory()) {
-                listDirectoryRecursive(file, level + 1);
-            }
-        }
-    }
-
-    private static void printFileInfo(File file, int level) {
-        String indent = " ".repeat(level * 2);
-        String type = file.isDirectory() ? "(D)" : "(F)";
-        String lastModified = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-                .format(new Date(file.lastModified()));
-        System.out.println(indent + file.getName() + " " + type + " " + lastModified);
-    }
-
 }
